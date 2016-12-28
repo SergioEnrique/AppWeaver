7879fbccde071367e3bf9e757a227fda354a22b4
RodrigoIbarraSanchez <lookatthemoon@live.com.mx>
2016-12-28 11:36:09 -0600

    avance de emit y on

--- js/controllers/resultados.js
+++ js/controllers/resultados.js
@@ -2,7 +2,12 @@
 
     angular
         .module("demoQuiz")
-        .controller("resultadosCtrl", ResultadosController)
+        .controller("controladorPadre", ['$scope', 'quizMetrics', 'DataService',
+            function($scope){
+            $scope.$on('algunEvento', function (evt, args) {
+                console.log("mensaje emitido!");
+            });
+        }])
 
         .controller('resultsCtrl', ['$scope', 'quizMetrics', 'DataService',
             function($scope, quizMetrics, DataService){
@@ -16,11 +21,12 @@
                 }
                 $scope.resultadosDelUsuario = quizMetrics.opcionUno();*/
             }
-        ]);
+        ])
 
-        ResultadosController.$inject = ['quizMetrics', 'DataService'];
-    
-        function ResultadosController(quizMetrics, DataService) {
+        /*ResultadosController.$inject = ['quizMetrics', 'DataService']*/
+
+        .controller("resultadosCtrl", ['quizMetrics', 'DataService', '$scope',
+            function (quizMetrics, DataService, $scope){
             var dq = this;
 
             dq.quizMetrics = quizMetrics;
@@ -30,6 +36,12 @@
             dq.reset = reset;
             dq.calcularPorcentaje = calcularPorcentaje;
             dq.preguntaActiva = 0;
+            dq.$scope = $scope;
+
+
+            $scope.emitMessage = function () {
+                $scope.$emit('algunEvento', { message: 'Mensaje Emitido!' });
+            }
             
             function calcularPorcentaje() {
                 return quizMetrics.numCorrect / DataService.preguntasQuiz.length * 100;
@@ -59,6 +71,6 @@
                 }
             }
 
-        }
+        }]);
 
 })();
